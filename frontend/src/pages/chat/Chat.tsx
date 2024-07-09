b2c49e833bfced163d8f7a3c48f8b7f101a7e786
Garrett McGrady <gmcgrady@secc.org>
2024-07-09 10:24:03 -0400

    ROCK-7003 Customizing front-end for SEGPT-Kuhl web app

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -767,8 +767,13 @@ const Chat = () => {
             {!messages || messages.length < 1 ? (
               <Stack className={styles.chatEmptyState}>
                 <img src={ui?.chat_logo ? ui.chat_logo : Contoso} className={styles.chatIcon} aria-hidden="true" />
+<<<<<<< Updated upstream
                 <h1 className={styles.chatEmptyStateTitle}>{ui?.chat_title}</h1>
                 <h2 className={styles.chatEmptyStateSubtitle}>{ui?.chat_description}</h2>
+=======
+                <h1 className={styles.chatEmptyStateTitle}>Sermon Search</h1>
+                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot has been trained on Carl Kuhl's sermons</h2>
+>>>>>>> Stashed changes
               </Stack>
             ) : (
               <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? '40px' : '0px' }} role="log">
