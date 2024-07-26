e0f455d9753ba6e44b4dbc9ec1ccd7fabeae3ddc
Garrett McGrady <gmcgrady@secc.org>
2024-07-26 17:03:50 -0400

    ROCK-7034 New erorr handling for line 463 of Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -461,11 +461,27 @@ const Chat = () => {
         errorMessage = parseErrorMessage(errorMessage)
 
         console.log('Error msg is on line 463');
-        let errorChatMsg: ChatMessage = {
+        /*let errorChatMsg: ChatMessage = {
           id: uuid(),
           role: ERROR,
           content: errorMessage,
           date: new Date().toISOString()
+        }*/
+        let errorChatMsg: ChatMessage;
+        if (errorMessage.includes("Unterminated string")) {
+            errorChatMsg = {
+                id: uuid(),
+                role: "ASSISTANT",
+                content: "Uh-oh! I seem to have lost my train of thought. Could you please reword your question and ask me again?",
+                date: new Date().toISOString()
+            };
+        } else {
+            errorChatMsg = {
+                id: uuid(),
+                role: "ERROR",
+                content: errorMessage,
+                date: new Date().toISOString()
+            };
         }
 
         let resultConversation
